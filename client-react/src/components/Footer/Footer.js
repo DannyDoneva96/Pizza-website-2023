229badfc75de7e2029d427f946c02cedfd619b5e
DannyDoneva96 <danielasinmas1@gmail.com>
2023-01-09 14:14:02 +0200

    finished the home page
    addooter and home content
    add css modules

--- client-react/src/components/Footer/Footer.js
+++ client-react/src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-
+import "./Footer.css"
 
 export const Footer = (props) => {
 
@@ -8,8 +8,17 @@ export const Footer = (props) => {
     return (
 
 
-        <footer>
-           
+           <footer>
+    <div className="footer">
+        <div className="socialMedia">
+            <i className="fa-brands fa-facebook"></i>
+            <i className="fa-brands fa-instagram"></i>
+            <i className="fa-brands fa-twitter"></i>
+            <i className="fa-brands fa-linkedin"></i>
+
+        </div>
+        <p> &copy; DDoneva 2023  project</p>
+    </div>
         </footer>)
 
 }
--- client-react/src/components/Home/Home.js
+++ client-react/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
-
+import combo from '../../images/pizza-images/deals/combo-4family.png'
+import './Home.css' 
 
 export const Home = (props) => {
 
@@ -9,7 +10,48 @@ export const Home = (props) => {
 
 
         <div>
-           mjkbhujkgbiu
+            <main>
+        <div className="main-home">
+          <div className="home">
+            <div className="main-content">
+              <h1> Donevis's Pizzeria </h1>
+              <p className="home-p"> Do good. Be nice. Order pizza. Repeat.</p>
+              <div className="order-btn-parrent">
+                <a className="order-btn glow-on-hover" href='/menu'> ORDER NOW </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <section className="home-deliv-section">
+          <h2>Easy in <span>three steps... </span></h2>
+          <div className="steps-container">
+            <div className="steps">
+              <p className="num">1</p>
+              <p>select your meal</p>
+            </div>
+            <div className="steps">
+              <p className="num">2</p>
+              <p>fill the details</p>
+            </div>
+            <div className="steps">
+              <p className="num">3</p>
+              <p>your order delivered in minute</p>
+            </div>
+          </div>
+          <div className="offer-of-the-day">
+            <div>
+              <h1 className="dealOfDay">
+                DEAL OF THE DAY
+              </h1>
+              <div className="img-container">
+                <a href="/menu/deals/Cl7TJYde1osByGBsOjrm">  <img src={combo} alt="" /></a>
+              </div>
+              <h3>Combo Deal</h3>
+              <p>pay<span>45.00$</span>instead of<span className="scratch">60.00$</span></p>
+            </div>
+          </div>
+        </section>
+      </main>
         </div>)
 
 }
