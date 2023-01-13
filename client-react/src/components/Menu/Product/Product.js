a3f6f135c5a5206befd88d50fce11fda14f7a7a1
DannyDoneva96 <danielasinmas1@gmail.com>
2023-01-13 12:26:31 +0200

    successfully navigate thru the swiper image

--- client-react/src/components/Menu/Product/Product.js
+++ client-react/src/components/Menu/Product/Product.js
@@ -1,13 +1,19 @@
 import './Product.css';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+
 
 export const Product = ({x}) => {
-   {console.log(x)}
+
+    const navigate = useNavigate();
+
+
+
     return(
       
          <article className = "pizza-article">
             <img src={x.imgUrl} alt="ghygyh" />
              <h3 className="pizza-name">{x.productName}</h3>
-             <button className="btn-o">Order</button>
+             <button onClick={  ()=>{ navigate(`/details/${x.id}`)}} className="btn-o">Order</button>
          </article>
     )
 }
