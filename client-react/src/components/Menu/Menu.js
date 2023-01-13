585b4374b019afe39d38622830f76f6cd11f2f08
DannyDoneva96 <danielasinmas1@gmail.com>
2023-01-13 12:15:14 +0200

    up

--- client-react/src/components/Menu/Menu.js
+++ client-react/src/components/Menu/Menu.js
@@ -87,7 +87,7 @@ export const Menu = (props) => {
           stretch: 0,
           depth: 100,
           modifier: 1,
-          slideShadows: true,
+          slideShadows: false,
         }}
         pagination={true}
         modules={[EffectCoverflow, Pagination]}
--- client-react/src/components/Menu/Product/Product.js
+++ client-react/src/components/Menu/Product/Product.js
@@ -1,4 +1,4 @@
-
+import './Product.css';
 
 export const Product = ({x}) => {
    {console.log(x)}
@@ -7,6 +7,7 @@ export const Product = ({x}) => {
          <article className = "pizza-article">
             <img src={x.imgUrl} alt="ghygyh" />
              <h3 className="pizza-name">{x.productName}</h3>
+             <button className="btn-o">Order</button>
          </article>
     )
 }
